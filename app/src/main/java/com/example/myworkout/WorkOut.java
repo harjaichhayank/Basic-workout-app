97a5efab70127f1b3d411ed49fa829f32dd7f8f4
Chhayank <harjaichhayank1@gmail.com>
2020-05-29 09:19:34 +0530

    changes --- from editText to TextView

--- app/src/main/java/com/example/myworkout/WorkOut.java
+++ app/src/main/java/com/example/myworkout/WorkOut.java
@@ -3,31 +3,32 @@ package com.example.myworkout;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WorkOut {
+class WorkOut {
 
-    List<String> getWorkouts (String workOutTypes)
-    {
+    List<String> getWorkouts (String workOutTypes) {
         List <String> workout = new ArrayList<>();
 
-        if(workOutTypes.equals("chest")) {
-            workout.add("Bench press");
-            workout.add("cable fly's");
-        }
-        else if(workOutTypes.equals("back")){
-            workout.add("Lat Pull Downs");
-        }
-        else if(workOutTypes.equals("biceps")){
-            workout.add("Bicep curls");
-        }
-        else if(workOutTypes.equals("triceps")) {
-            workout.add("TriCep Ext");
-            workout.add("TriCep PushDowns");
-        }
-        else if(workOutTypes.equals("legs")){
-            workout.add("leg press");
-        }
-        else if(workOutTypes.equals("abs")){
-            workout.add("dragon fly's");
+        switch (workOutTypes) {
+            case "chest":
+                workout.add("Bench press");
+                workout.add("cable fly's");
+                break;
+            case "back":
+                workout.add("Lat Pull Downs");
+                break;
+            case "biceps":
+                workout.add("Bicep curls");
+                break;
+            case "triceps":
+                workout.add("TriCep Ext");
+                workout.add("TriCep PushDowns");
+                break;
+            case "legs":
+                workout.add("leg press");
+                break;
+            case "abs":
+                workout.add("dragon fly's");
+                break;
         }
         return workout;
     }
